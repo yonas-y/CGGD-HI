91f9785529bb7ab22c8238a9854957d417a5946f
Yonas Y <yoni99y@gmail.com>
2025-07-01 16:18:54 +0300

    Created a config file to be used by other scripts!

---
+++ app/config.py
@@ -0,0 +1,13 @@
+"""
+Contains Configuration (DB URI, names, paths).
+"""
+
+# app/config.py
+MONGO_URI = "mongodb://192.168.80.1:27017"  # The address to the local MongoDB!
+HOST_IP = "192.168.80.1"
+HOST_PORT = 27017
+# DB_NAME = "Customers-Satisfaction"
+# COLLECTION_NAME = "Customers"
+MODEL_DIR = "output"
+# MODEL_PATH = f"{MODEL_DIR}/ocsvm_model.pkl"
+# SCALER_PATH = f"{MODEL_DIR}/scaler.pkl"
