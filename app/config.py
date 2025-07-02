d37680b6323cb41dfbafeb66c3472754e30668c1
Yonas Y <yoni99y@gmail.com>
2025-07-02 12:09:10 +0300

    Updated the config file with path to raw training and test data!

--- app/config.py
+++ app/config.py
@@ -3,11 +3,9 @@ Contains Configuration (DB URI, names, paths).
 """
 
 # app/config.py
-MONGO_URI = "mongodb://192.168.80.1:27017"  # The address to the local MongoDB!
-HOST_IP = "192.168.80.1"
-HOST_PORT = 27017
-# DB_NAME = "Customers-Satisfaction"
-# COLLECTION_NAME = "Customers"
 MODEL_DIR = "output"
+TRAIN_DATA_DIR = "data/raw/Pronostia/Learning_set"
+TEST_DATA_DIR = "data/raw/Pronostia/Test_set"
+FEATURE_DIR = "data/features"
 # MODEL_PATH = f"{MODEL_DIR}/ocsvm_model.pkl"
 # SCALER_PATH = f"{MODEL_DIR}/scaler.pkl"
