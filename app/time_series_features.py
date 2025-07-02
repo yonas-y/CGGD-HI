6b4903ed797995bf5910489708feb3bd80a1c8f1
Yonas Y <yoni99y@gmail.com>
2025-07-02 23:38:03 +0300

    Created a class for time-series signal features!

---
+++ app/time_series_features.py
@@ -0,0 +1,22 @@
+import numpy as np
+import librosa
+import pywt
+
+
+class Signal_Features:
+    """
+        A class used for time series signal feature extraction.
+    """
+    def __init__(self, signal, sample_rate, frame_length, hop_length):
+        """
+        Args:
+            signal: Time series signal.
+            sample_rate: Signal sampling rate.
+            frame_length: Frame length.
+            hop_length: Hop length.
+        """
+        # Load the audio signal.
+        self.vib_signal = signal.astype('float64')
+        self.sample_rate = sample_rate
+        self.frame_length = frame_length
+        self.hop_length = hop_length
