12645fdcc796f0869f03daa7f9ea31c349db7acd
Yonas Y <yoni99y@gmail.com>
2025-07-02 12:28:50 +0300

    Updated to include the path to the pickle directory!

--- app/config.py
+++ app/config.py
@@ -4,8 +4,9 @@ Contains Configuration (DB URI, names, paths).
 
 # app/config.py
 MODEL_DIR = "output"
-TRAIN_DATA_DIR = "data/raw/Pronostia/Learning_set"
-TEST_DATA_DIR = "data/raw/Pronostia/Test_set"
+TRAIN_RAW_DATA_DIR = "data/raw/Pronostia/Learning_set"
+TEST_RAW_DATA_DIR = "data/raw/Pronostia/Test_set"
+PICKLE_DIR = "data/pickles"
 FEATURE_DIR = "data/features"
 # MODEL_PATH = f"{MODEL_DIR}/ocsvm_model.pkl"
 # SCALER_PATH = f"{MODEL_DIR}/scaler.pkl"
