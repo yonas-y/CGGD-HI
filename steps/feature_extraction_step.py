db467a78067fc934c4459f760ec016fb6d4ecf4d
Yonas Y <yoni99y@gmail.com>
2025-07-03 12:16:01 +0300

    Created a feature extraction step!

---
+++ steps/feature_extraction_step.py
@@ -0,0 +1,17 @@
+from zenml import step
+import os
+import logging
+
+from app.feature_extraction import feature_extraction
+
+logger = logging.getLogger(__name__)
+
+@step
+def feature_extraction_step(pickle_dir, feature_dir) -> None:
+    """
+    Step to extract features from the input pickle data.
+    """
+    logger.info(f"📦 Extracting features ...")
+    feature_extraction(pickle_dir, feature_dir)
+
+    return
