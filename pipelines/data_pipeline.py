66d310701bcd0a1b1b4adade682d3458dc7550e9
Yonas Y <yoni99y@gmail.com>
2025-07-03 12:28:50 +0300

    Added a feature extraction step in the pipeline!

--- pipelines/data_pipeline.py
+++ pipelines/data_pipeline.py
@@ -1,7 +1,16 @@
 from zenml import pipeline
-from steps.data_import_step import import_and_catch_data
-from app.config import TRAIN_RAW_DATA_DIR, TEST_RAW_DATA_DIR, PICKLE_TRAIN_DIR, PICKLE_TEST_DIR
+from steps.data_import_step import import_and_catch_data_step
+from steps.feature_extraction_step import feature_extraction_step
+from app.config import TRAIN_RAW_DATA_DIR, TEST_RAW_DATA_DIR, PICKLE_TRAIN_DIR, PICKLE_TEST_DIR, FEATURE_DIR
+
 @pipeline
 def data_pipeline():
-    import_and_catch_data(TRAIN_RAW_DATA_DIR, PICKLE_TRAIN_DIR)   # Import the training set!
-    import_and_catch_data(TEST_RAW_DATA_DIR, PICKLE_TEST_DIR)    # Import the test set!
+    # Raw data importing step!
+    import_and_catch_data_step(TRAIN_RAW_DATA_DIR, PICKLE_TRAIN_DIR)   # Import the training set!
+    import_and_catch_data_step(TEST_RAW_DATA_DIR, PICKLE_TEST_DIR)    # Import the test set!
+
+    # Feature extraction step!
+    feature_extraction_step(PICKLE_TRAIN_DIR, FEATURE_DIR) # Feature extraction for the training set!
+    feature_extraction_step(PICKLE_TEST_DIR, FEATURE_DIR) # Feature extraction for the test set!
+
+
