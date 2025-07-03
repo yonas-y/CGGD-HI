f278d4ceba59a421ea7f0c6c456683ebd7df6db6
Yonas Y <yoni99y@gmail.com>
2025-07-03 10:26:15 +0300

    Added parameter information about pronostia dataset!

--- app/config.py
+++ app/config.py
@@ -1,8 +1,11 @@
 """
-Contains Configuration (DB URI, names, paths).
+Contains Configuration (data, names, paths, .....).
 """
 
-# app/config.py
+# ========== Directories!!! ============== #
+"""
+Directories for Data, Features, Model Parameters and ....
+"""
 MODEL_DIR = "output"
 TRAIN_RAW_DATA_DIR = "data/raw/Pronostia/Learning_set"
 TEST_RAW_DATA_DIR = "data/raw/Pronostia/Test_set"
@@ -11,3 +14,14 @@ PICKLE_TEST_DIR = "data/pickles/test"
 FEATURE_DIR = "data/features"
 # MODEL_PATH = f"{MODEL_DIR}/ocsvm_model.pkl"
 # SCALER_PATH = f"{MODEL_DIR}/scaler.pkl"
+
+
+# ========== Parameters from Pronostia dataset! ================== #
+"""
+Important parameters about the Pronostia dataset!!
+"""
+SampleRate = 25600                          # sampling rate of the incoming signal
+OneS_Samples = 2560                         # samples representative of 1 sec duration.
+frame_length = 2560                         # number of FFT components
+hop_length = 2561                           # hop length for spectrogram frames
+n_mels = 256                                # number of Mel bands to generate
