57510668c724f392a74c035a3968a6441bb89409
Yonas Y <yoni99y@gmail.com>
2025-07-03 12:06:40 +0300

    Updated the feature output path!

--- app/config.py
+++ app/config.py
@@ -11,7 +11,7 @@ TRAIN_RAW_DATA_DIR = "data/raw/Pronostia/Learning_set"
 TEST_RAW_DATA_DIR = "data/raw/Pronostia/Test_set"
 PICKLE_TRAIN_DIR = "data/pickles/training"
 PICKLE_TEST_DIR = "data/pickles/test"
-FEATURE_DIR = "data/features"
+FEATURE_DIR = "data/features/mel_features"
 # MODEL_PATH = f"{MODEL_DIR}/ocsvm_model.pkl"
 # SCALER_PATH = f"{MODEL_DIR}/scaler.pkl"
 
