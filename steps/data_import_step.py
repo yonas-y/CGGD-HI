01b94409bb43b6683f5d7e8eb747177d1d0a5a9d
Yonas Y <yoni99y@gmail.com>
2025-07-07 00:43:43 +0300

    Removed the condition of checking the folder!

--- steps/data_import_step.py
+++ steps/data_import_step.py
@@ -9,12 +9,9 @@ logger = logging.getLogger(__name__)
 @step(enable_cache=True)
 def import_and_catch_data_step(raw_data_dir, pickle_dir) -> None:
     """
-    Step to load raw CSVs, combine and save as pickle, only if pickle doesn't already exist.
+    Step to load raw CSVs, combine and save as pickle.
     """
-    if os.path.exists(pickle_dir):
-        logger.info(f"✅ Pickle already exists at {pickle_dir}, skipping import.")
-    else:
-        logger.info(f"📦 Pickle not found at {pickle_dir}. Importing raw data...")
-        import_bearing_data_to_pickle(raw_data_dir, pickle_dir)
+    logger.info("📦 Importing raw data as a pickle file...")
+    import_bearing_data_to_pickle(raw_data_dir, pickle_dir)
 
     return
