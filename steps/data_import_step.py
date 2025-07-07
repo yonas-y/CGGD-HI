2d7d9aeeccaf280323457e6cda8a2c5cff9b7476
Yonas Y <yoni99y@gmail.com>
2025-07-07 21:27:02 +0300

    Updated the step to be usable for the two setups!

--- steps/data_import_step.py
+++ steps/data_import_step.py
@@ -2,16 +2,20 @@ from zenml import step
 import os
 import logging
 
-from app.data_importing import import_bearing_data_to_pickle
+from app.data_importing import import_pronostia_data_to_pickle, import_XJTU_SY_data_to_pickle
 
 logger = logging.getLogger(__name__)
 
 @step(enable_cache=True)
-def import_and_catch_data_step(raw_data_dir, pickle_dir) -> None:
+def import_and_catch_data_step(setup_name, raw_data_dir, pickle_dir) -> None:
     """
     Step to load raw CSVs, combine and save as pickle.
     """
-    logger.info("📦 Importing raw data as a pickle file...")
-    import_bearing_data_to_pickle(raw_data_dir, pickle_dir)
+    if setup_name == "pronostia":
+        logger.info("📦 Importing pronostia raw data as a pickle file...")
+        import_pronostia_data_to_pickle(raw_data_dir, pickle_dir)
+    elif setup_name == "XJTU_SY":
+        logger.info("📦 Importing XJTU_SY raw data as a pickle file...")
+        import_XJTU_SY_data_to_pickle(raw_data_dir, pickle_dir)
 
     return
