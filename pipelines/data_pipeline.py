4d39c54732bbc6baaacc0a3a046a1b97f747583f
Yonas Y <yoni99y@gmail.com>
2025-07-07 09:17:47 +0300

    Updated how the config script is called and its use in the different steps of the pipeline!

--- pipelines/data_pipeline.py
+++ pipelines/data_pipeline.py
@@ -1,24 +1,23 @@
 from zenml import pipeline
 from steps.data_import_step import import_and_catch_data_step
 from steps.feature_extraction_preprocessing_step import feature_extraction_step, feature_preprocessing_step
-from app.config import TRAIN_RAW_DATA_DIR, TEST_RAW_DATA_DIR, PICKLE_TRAIN_DIR, PICKLE_TEST_DIR
-from app.config import FEATURE_DIR, setup, channel
+from app.active_config import cfg
 
 @pipeline
 def data_pipeline():
     # Raw data importing step!
-    import_and_catch_data_step(TRAIN_RAW_DATA_DIR, PICKLE_TRAIN_DIR)   # Import the training set!
-    import_and_catch_data_step(TEST_RAW_DATA_DIR, PICKLE_TEST_DIR)    # Import the test set!
+    import_and_catch_data_step(cfg.TRAIN_RAW_DATA_DIR, cfg.PICKLE_TRAIN_DIR)   # Import the training set!
+    import_and_catch_data_step(cfg.TEST_RAW_DATA_DIR, cfg.PICKLE_TEST_DIR)    # Import the test set!
 
     # Feature extraction step!
-    feature_extraction_step(PICKLE_TRAIN_DIR, FEATURE_DIR) # Feature extraction for the training set!
-    feature_extraction_step(PICKLE_TEST_DIR, FEATURE_DIR) # Feature extraction for the test set!
+    feature_extraction_step(cfg.PICKLE_TRAIN_DIR, cfg.FEATURE_DIR) # Feature extraction for the training set!
+    feature_extraction_step(cfg.PICKLE_TEST_DIR, cfg.FEATURE_DIR) # Feature extraction for the test set!
 
     # Feature Preprocessing step!
     X_train_scaled, Ene_RUL_order_train, X_test_scaled = feature_preprocessing_step(
-        feature_directory=FEATURE_DIR,
-        setup_used=setup,
-        channel_used=channel
+        feature_directory=cfg.FEATURE_DIR,
+        bearing_used=cfg.bearing_used,
+        channel_used=cfg.channel
     )
 
     return X_train_scaled, Ene_RUL_order_train, X_test_scaled
