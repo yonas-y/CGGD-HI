ca94ec558421a18fe7257c2b52cbc0c7665ab034
Yonas Y <yoni99y@gmail.com>
2025-07-11 10:14:19 +0300

    Created a training pipeline script!

---
+++ pipelines/training_pipeline.py
@@ -0,0 +1,15 @@
+from zenml import pipeline
+
+from app.active_config import cfg
+
+@pipeline
+def training_pipeline():
+    # # Model development step!
+
+
+    # # Model training step!
+    # feature_extraction_step(cfg.PICKLE_DATA_DIR, cfg.FEATURE_DIR)
+
+
+
+    return
