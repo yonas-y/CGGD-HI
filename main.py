fd005fac2cefedb2f7b6988b1586954b06462d41
Yonas Y <yoni99y@gmail.com>
2025-07-14 20:26:23 +0300

    Loads the model development pipeline and executes the model using the training pipeline!

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from pipelines.data_pipeline import data_pipeline
+from zenml.client import Client
+from pipelines.training_pipeline import training_pipeline
+from pipelines.model_development_pipeline import model_development_pipeline
+
+from app.active_config import cfg
+
+import logging
+logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    # Run the data import, feature extract, preprocess pipeline!
-    data_outputs = data_pipeline()
+    # -----------------------------------------------
+    # ✅ Step 1: Building the model architecture used!
+    # -----------------------------------------------
+    model_run = model_development_pipeline()
+    client = Client()
 
-    # Run model training pipeline!
+    run = client.get_pipeline_run(str(model_run.id))
+    step_run = run.steps["model_development_step"]
+    convAE_model_artifact = step_run.outputs["output"][0]
+
+    # -----------------------------------------------
+    # ✅ Step 2: Load the data, train and validate!
+    # -----------------------------------------------
+    n_iters = cfg.model_training_params.training_iterations
+    for iter_n in range(n_iters):
+        logger.info(f"🚀 Started model execution step {iter_n + 1} ...... ")
+        training_pipeline(model=convAE_model_artifact, iter_n=iter_n)
