9ad46247889ea2bf8e26743ed4955d58668c0161
Yonas Y <yoni99y@gmail.com>
2025-07-14 20:23:37 +0300

    Added the iteration number as an argument of the step to be used in saving the model!

--- steps/training_step.py
+++ steps/training_step.py
@@ -10,7 +10,8 @@ logger = logging.getLogger(__name__)
 @step(enable_cache=False)
 def model_execution_step(model,
                          training_data: List,
-                         validation_data: List) -> pd.DataFrame:
+                         validation_data: List,
+                         iteration_n: int) -> pd.DataFrame:
     """
     ZenML step that executed the model using the COnvAE architecture
     for health indicator (HI) estimation.
@@ -21,6 +22,7 @@ def model_execution_step(model,
     logger.info(f"🧰 Initializing the model execution phase!")
     model_performance_df = model_execution(model=model,
                                            training_data=training_data,
-                                           validation_data=validation_data)
+                                           validation_data=validation_data,
+                                           iteration=iteration_n)
 
     return model_performance_df
