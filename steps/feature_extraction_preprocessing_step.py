bdc649f1c023283f6ec40b8b31089245b0346dd6
Yonas Y <yoni99y@gmail.com>
2025-07-14 15:35:37 +0300

    Added the data materializer to access the outputs of the steps in this script!

--- steps/feature_extraction_preprocessing_step.py
+++ steps/feature_extraction_preprocessing_step.py
@@ -2,8 +2,9 @@ from zenml import step
 import os
 import logging
 import numpy as np
-from typing import List, Tuple
+from typing import List, Tuple, Annotated
 from pathlib import Path
+from app.materializers.joblib_list_materializer import JoblibListMaterializer
 
 from app.feature_extraction import feature_extraction
 from app.feature_preprocessing import feature_preprocessing, features_ene_rul_train
@@ -23,12 +24,18 @@ def feature_extraction_step(pickle_dir: Path, feature_dir: Path) -> None:
 
     return
 
-@step(enable_cache=False)
+@step(enable_cache=False,
+      output_materializers={
+          "train_data_scaled": JoblibListMaterializer,
+          "train_Ene_RUL_Order": JoblibListMaterializer,
+          "test_data_scaled": JoblibListMaterializer
+      }
+      )
 def feature_preprocessing_step(feature_directory: Path, output_directory: Path,
                                bearing_used: str, channel_used: str) -> Tuple[
-    List[np.ndarray],
-    List[np.ndarray],
-    List[np.ndarray]
+    Annotated[List[np.ndarray], "train_data_scaled"],
+    Annotated[List[np.ndarray], "train_Ene_RUL_Order"],
+    Annotated[List[np.ndarray], "test_data_scaled"]
 ]:
     """
     Load mel features from the feature directory, then split and scale them
@@ -62,18 +69,33 @@ def feature_preprocessing_step(feature_directory: Path, output_directory: Path,
     logger.info("Feature preprocessing completed successfully.")
     return X_train_scaled, Ene_RUL_order_train, X_test_scaled
 
-@step(enable_cache=False)
+@step(enable_cache=False,
+      output_materializers={
+          "X_train_lists": JoblibListMaterializer
+      }
+      )
 def feature_partitioning_step(feature_mel: List[np.ndarray],
                               feature_ene_rul_order: List[np.ndarray],
-                              percentages: List[float]) -> List[List[np.ndarray]]:
+                              percentages: List[float]) -> Annotated[List[List[np.ndarray]], "X_train_lists"]:
     # Partitions the features into start, mid and final sections segments of the run!
     X_train_lists = create_feature_portions(feature_mel, feature_ene_rul_order, percentages)
 
     return X_train_lists
 
-@step(enable_cache=False)
-def train_validation_split_step(percentage_partitioned_data: List[List[np.ndarray]],
-    batch_percentages: List[float], val_split: float, batch_size: int)-> Tuple[List, List]:
+@step(enable_cache=False,
+      output_materializers={
+          "training_data": JoblibListMaterializer,
+          "validation_data": JoblibListMaterializer
+      }
+      )
+def train_validation_split_step(
+        percentage_partitioned_data: List[List[np.ndarray]],
+        batch_percentages: List[float],
+        val_split: float,
+        batch_size: int
+)-> Tuple[
+    Annotated[List, "training_data"],
+    Annotated[List, "validation_data"]]:
 
     # Shuffles and partitions the data into training and validation sets!
     training_data, validation_data = shuffle_batched_interleaved(percentage_partitioned_data,
