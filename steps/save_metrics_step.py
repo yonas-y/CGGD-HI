55231ea7d31783766c6ef5d984b2ae4ff7813a62
Yonas Y <yoni99y@gmail.com>
2025-07-14 19:32:12 +0300

    Created a separate step to save the model performance!

---
+++ steps/save_metrics_step.py
@@ -0,0 +1,26 @@
+from zenml import step
+import os
+import pandas as pd
+
+from app.active_config import cfg
+
+import logging
+logger = logging.getLogger(__name__)
+
+@step(enable_cache=False)
+def save_metrics_step(df: pd.DataFrame, iter_n: int):
+    output_dir = 'output/model_performance'
+    os.makedirs(output_dir, exist_ok=True)
+    filename = f'performance_df_' \
+               f'{cfg.model_hyperparams.encoding_n}_' \
+               f'{cfg.SETUP_Name}_' \
+               f'{cfg.constraint_params.reconstruction_rf}_' \
+               f'{cfg.constraint_params.soft_rank_rf}_' \
+               f'{cfg.constraint_params.monotonicity_rf[1]}_' \
+               f'{cfg.constraint_params.energy_hi_dev_rf}_' \
+               f'{cfg.constraint_params.upper_bound_rf}_' \
+               f'{cfg.constraint_params.lower_bound_rf}_' \
+               f'{iter_n}.pkl'
+
+    df.to_pickle(os.path.join(output_dir, filename))
+    logger.info(f"✅ Saved performance DataFrame as {filename}")
