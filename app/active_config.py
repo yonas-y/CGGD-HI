74123baa54db83e92d581965d28849415ffd9c41
Yonas Y <yoni99y@gmail.com>
2025-07-15 16:13:58 +0300

    The common setup configurations when training in non-constrained way.

--- app/active_config.py
+++ app/active_config.py
@@ -1,10 +1,22 @@
 # active_config.py
 import tensorflow as tf
 from app.config import get_config, update_config
+import os
 
-SETUP = "pronostia"        # "pronostia or XJTU_SY"
+SETUP = "XJTU_SY"         # "pronostia or XJTU_SY"
+Local = False                # True=local, False=remote
+
+if not Local:
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+    os.environ["NUMEXPR_MAX_THREADS"] = "32"
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+
 # Get the base config for the chosen setup
-cfg = get_config(SETUP)
+cfg = get_config(setup_name=SETUP, Local=Local)
 
 # Apply dynamic update immediately
 update_config(cfg,
@@ -15,11 +27,10 @@ update_config(cfg,
               n_mels=128)
 
 # ======== Change model hyperparameters when necessary! ========= #
-cfg.model_hyperparams.encoding_n = 64
+cfg.model_hyperparams.encoding_n = 16
 cfg.model_hyperparams.dropout_rate = 0.0
 cfg.model_hyperparams.regularization = 1e-3
-cfg.model_hyperparams.activation_function = tf.keras.layers.LeakyReLU(negative_slope=0.0)
-
+cfg.model_hyperparams.activation_function = tf.keras.layers.LeakyReLU(alpha=0.0)
 
 # ======== Update the model training parameters when necessary! ========= #
 # Change the segment partition percentage if necessary!
@@ -28,18 +39,17 @@ cfg.model_training_params.run_partitioning_portion = (0.1, 0.85, 0.05)  # first 
 # Change the segment percentages which makes a batch if necessary!
 cfg.model_training_params.segment_percentages_in_batch = (0.2, 0.7, 0.1)
 
-cfg.model_training_params.batch_size = 64
-cfg.model_training_params.validation_split = 0.2
-cfg.model_training_params.epochs = 100
-cfg.model_training_params.patience = 15
 cfg.model_training_params.training_iterations = 10
-cfg.model_training_params.save_weights = False
-cfg.model_training_params.early_stop = False
-
+cfg.model_training_params.epochs = 150
+cfg.model_training_params.patience = 15
+cfg.model_training_params.batch_size = 16
+cfg.model_training_params.validation_split = 0.1
+cfg.model_training_params.save_weights = True
+cfg.model_training_params.early_stop = True
 
 # ======== Update the rescale factors of the constraints ========= #
-cfg.constraint_params.soft_rank_rf = 1.0
-cfg.constraint_params.monotonicity_rf = (1.25, 1.5)
-cfg.constraint_params.energy_hi_dev_rf=1.5
-cfg.constraint_params.upper_bound_rf=2.0
-cfg.constraint_params.lower_bound_rf=2.0
+cfg.constraint_params.soft_rank_rf = 0.0
+cfg.constraint_params.monotonicity_rf = (0.0, 0.0)
+cfg.constraint_params.energy_hi_dev_rf = 0.0
+cfg.constraint_params.upper_bound_rf = 0.0
+cfg.constraint_params.lower_bound_rf = 0.0
